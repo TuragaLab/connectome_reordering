7d1420e97f707743b2fa81ca41fd079d115c2028
0xJustin <justinmjoyce75@gmail.com>
2024-10-29 11:45:39 -0400

    simplified embedding reorder

--- reorder_cluster.py
+++ reorder_cluster.py
@@ -25,74 +25,68 @@ def run(run_idx):
     source_indices = jnp.array([node_id_to_index[node_id] for node_id in source_nodes])
     target_indices = jnp.array([node_id_to_index[node_id] for node_id in target_nodes])
     edge_weights = jnp.array(edge_weights)
-
-    # Compute total edge weight
     max_edge_weight = jnp.max(edge_weights)
 
     # Normalize edge weights
     edge_weights = edge_weights / max_edge_weight
 
     num_nodes = len(unique_nodes)
-    key = random.PRNGKey(int(run_idx)+52345235)
-    embedding_dim = 15  # Adjust the embedding dimensionality
-    positions = random.uniform(
-        key, shape=(num_nodes, embedding_dim), minval=-1, maxval=1
-    )
-    key, subkey = random.split(key)
-    w = random.uniform(subkey, shape=(embedding_dim,))
+    key = random.PRNGKey(int(run_idx)+220)
+    # How to initialize positions - uniform or normal distributions seem reasonable
+    positions = random.normal(key, shape=(num_nodes,))
 
-    # Define the optimizer with gradient clipping)
-#    positions = jnp.load("./checkpoints/positions_82.63022402029044_4.npy")
-#    w = jnp.load("./checkpoints/weights_82.63022402029044_4.npy")
+    # Hyper parameter
+    num_epochs = 20000
 
-    num_epochs = 100000
-    beta = jnp.logspace(-1, 1, num=num_epochs)
-    exponential_decay_scheduler = optax.exponential_decay(init_value=0.0005, transition_steps=num_epochs,
-                                                      decay_rate=0.995, transition_begin=int(num_epochs*0.5),
-                                                      staircase=False)
-    #exponential_decay_scheduler = optax.cosine_decay_schedule(init_value=0.0005, decay_steps=num_epochs)
-
-    optimizer = optax.adam(learning_rate=exponential_decay_scheduler)
+    exponential_decay_scheduler = optax.exponential_decay(init_value=.05, transition_steps=num_epochs,
+                                                        decay_rate=0.1, transition_begin=int(num_epochs*0.5),
+                                                        staircase=True)
     optimizer = optax.chain(
-        optax.clip_by_global_norm(1.0),
-        optax.adam(learning_rate=exponential_decay_scheduler)
+        optax.clip_by_global_norm(1.0), optax.adamw(learning_rate=exponential_decay_scheduler)
     )
-    # Initialize optimizer state
-    opt_state = optimizer.init((positions, w))
-
+    best_positions = positions
+    n_refresh = 1
     best_metric = 0
 
     @jax.jit
-    def optimization_step(
-        positions, w,beta, opt_state, source_indices, target_indices, edge_weights
-    ):
-        # Compute loss and gradients for both positions and w
-        loss, grads = jax.value_and_grad(functions.objective_function, argnums=(0, 1))(
-            positions, w, beta, source_indices, target_indices, edge_weights
+    def optimization_step(relu_weight, positions,beta, opt_state,edge_weights):
+        params = (positions)#(positions, w)
+
+        loss, grads = jax.value_and_grad(functions.objective_function, argnums=(1))(
+            relu_weight, positions, beta, source_indices, target_indices, edge_weights
         )
 
         # Update positions and w
-        updates, opt_state = optimizer.update(grads, opt_state)
-        positions, w = optax.apply_updates((positions, w), updates)
+        updates, opt_state = optimizer.update(grads, opt_state, params)
+        positions= optax.apply_updates(params, updates)
+        return positions, opt_state, loss
+    for itr in range(n_refresh):
+    # Initialize optimizer state
+        positions = best_positions
+        opt_state = optimizer.init(positions)
+        n_cycles = 5
 
-        return positions, w, opt_state, loss
-    for epoch in range(num_epochs):
-        positions, w, opt_state, loss = optimization_step(
-            positions, w, beta[epoch], opt_state, source_indices, target_indices, edge_weights
-        )
+        betas = (jnp.cos(np.linspace(0, 2 * n_cycles * np.pi, num_epochs)) + 1.1) / 2
+        # Create a wrapper for the objective function to fix additional arguments
 
-        # Optional: Print progress every 100 epochs
-        if epoch % 100 == 0:
-            print(f"Epoch {epoch}, Loss: {-loss}")
+        for epoch in range(num_epochs):
+            key, subkey = random.split(key)
+            noise = jax.random.normal(subkey, (positions.shape[0], ))
+            positions, opt_state, loss = optimization_step(
+                0, positions, betas[epoch], opt_state,edge_weights
+            )
+
             metric = functions.calculate_metric(
-                positions, w, num_nodes, source_indices, target_indices, edge_weights
+                positions, num_nodes, source_indices, target_indices, edge_weights
             )
             if metric > best_metric:
                 best_metric = metric
+                best_positions = positions
                 print(f"New best metric: {best_metric:.2f}")
-
+            if epoch % 2000 == 0:
+                print(itr, f"Epoch {epoch}, Loss: {loss}, Best metric: {best_metric:.2f}, Beta: {betas[epoch]:.2f}")
     # Map back to original node IDs and save the ordering
-    sorted_indices = jnp.argsort(np.dot(positions, w))
+    sorted_indices = jnp.argsort(best_positions)
     ordered_node_ids = [index_to_node_id[int(idx)] for idx in sorted_indices]
 
     # Save the ordering to a CSV file
@@ -100,8 +94,8 @@ def run(run_idx):
     ordered_nodes_df = pd.DataFrame({"Node ID": ordered_node_ids, "Order": jnp.arange(num_nodes)})
     ordered_nodes_df.to_csv(f"./checkpoints/ordered_nodes_{best_metric}_{run_idx}.csv", index=False)
     # Save weights
-    jnp.save(f'./checkpoints/weights_{best_metric}_{run_idx}.npy', w)
-    jnp.save(f'./checkpoints/positions_{best_metric}_{run_idx}.npy', positions)
+    #jnp.save(f'./checkpoints/weights_{best_metric}_{run_idx}.npy', w)
+    jnp.save(f'./checkpoints/positions_{best_metric}_{run_idx}.npy', best_positions)
 
 if __name__ == "__main__":
     import sys
